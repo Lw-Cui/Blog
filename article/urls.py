06933953818bcfede7a5e5dc72df2b2ba494f95d
Lw-Cui <645739460@QQ.com>
2015-07-10 08:23:22 +0800

    collectstatic

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
 from .views import *
 
-urlpatterns = patterns('hellolw.blog.views',
+urlpatterns = patterns('',
 	url(r'^$', home),
 	url(r'^(?P<blog_id>\d+)/(?P<blog_link>[\w,-]*)$', 'blog_detail', name='blog_detail'),
 	url(r'^archives/(?P<page_num>\d?)$', 'archives', name='archives'),
