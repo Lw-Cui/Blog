c0a2c52b033576ff713c1098b5f1fe2360e4d7bf
Lw-Cui <645739460@QQ.com>
2015-07-14 00:07:10 +0800

    finish markdown @content and index

--- article/views.py
+++ article/views.py
@@ -5,6 +5,10 @@ from blog import settings
 from markdown import markdown2
 import datetime
 
+def render_post(post):
+	post.content = markdown2.markdown(post.content)
+	return post
+
 def home(request, page_num=1):
 	now = datetime.datetime.now()
 	if now.hour <= 7:
@@ -16,15 +20,15 @@ def home(request, page_num=1):
 	else:
 		greet = 'Have a good dream, lw.'
 
-	for post in article.objects.all():
-	    post.content = markdown2.markdown(post.content)
-	paginator = Paginator(article.objects.all(), settings.PAGE_NUM)
+	post_list = list(map(render_post, article.objects.all()))
+	paginator = Paginator(post_list, settings.PAGE_NUM)
+	#paginator = Paginator(article.objects.all(), settings.PAGE_NUM)
 	return render_to_response('index.html', {'blogs': paginator.page(page_num), 'greet': greet})
 
 def content(request, blogID):
-    post = article.objects.get(pk=blogID)
-    post.content = markdown2.markdown(post.content)
-    return render_to_response('content.html', {'blog': post, 'blogs': article.objects.all()})
+	post = article.objects.get(pk=blogID)
+	post.content = markdown2.markdown(post.content)
+	return render_to_response('content.html', {'blog': post, 'blogs': article.objects.all()})
 
 def about(request):
 	return render_to_response('about.html', {'blogs': article.objects.all()})
