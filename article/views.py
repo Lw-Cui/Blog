f2ace6f894619e2c9c59b47aacbaeb1fcf154a13
Lw-Cui <645739460@QQ.com>
2015-07-18 17:43:12 +0800

    tiny jpg

--- article/views.py
+++ article/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import loader, RequestContext
 from django.core.paginator import Paginator
 from models import *
 from blog import settings
@@ -20,14 +21,19 @@ def present():
     else:
         return 'evening'
 
+def temp_proc(request):
+    return {
+        'tags': tag.objects.all(),
+        'name': present(),
+    }
+
 def tagArchives(request, tagId, page_num = 1):
     post_list = list(map(render_post, article.objects.filter(tags__id = tagId)))
     paginator = Paginator(post_list, settings.PAGE_NUM)
     dict = {'blogs': paginator.page(page_num),
-            'tag': tag.objects.get(id = tagId),
-            'name': present()}
-    dict['tags'] = tag.objects.all()
-    return render_to_response('tagArchives.html', dict)
+            'tag': tag.objects.get(id = tagId)}
+    return render_to_response('tagArchives.html', dict,
+                              context_instance = RequestContext(request, processors=[temp_proc]))
 
 def home(request, page_num = 1):
     if present() == 'dawn':
@@ -42,19 +48,17 @@ def home(request, page_num = 1):
     post_list = list(map(render_post, article.objects.all()))
     paginator = Paginator(post_list, settings.PAGE_NUM)
     dict = {'blogs': paginator.page(page_num),
-            'greet': greet,
-            'name': present()}
-    dict['tags'] = tag.objects.all()
-    return render_to_response('home.html', dict)
+            'greet': greet,}
+    return render_to_response('home.html', dict,
+                              context_instance = RequestContext(request, processors=[temp_proc]))
 
 def content(request, blogID):
     post = article.objects.get(pk=blogID)
     post.content = markdown2.markdown(post.content)
-    dict = {'blog': post,
-            'name': present()}
-    dict['tags']  = tag.objects.all()
-    return render_to_response('content.html', dict)
+    dict = {'blog': post,}
+    return render_to_response('content.html', dict,
+                              context_instance = RequestContext(request, processors=[temp_proc]))
 
 def about(request):
-    return render_to_response('about.html', {'tags':tag.objects.all(),
-                                             'name':present()})
+    return render_to_response('about.html', {},
+                              context_instance = RequestContext(request, processors=[temp_proc]))
